85c35fd4fefe258d8ffcba703928ea0f90b472d9
Nadav Mavor <nadav.mavor@gmail.com>
2019-11-16 12:47:39 -0500

    switc from  BOARD_RAMPS_13_EFB to BOARD_RAMPS_14_EFB

--- Configuration.h
+++ Configuration.h
@@ -131,7 +131,7 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_13_EFB
+  #define MOTHERBOARD BOARD_RAMPS_14_EFB
   
 #endif
 
